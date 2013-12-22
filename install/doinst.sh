f6f55dbb5de9af0b8e83af2a6e2bdf88b9b4a637
Nic Aitch <nic@nicinabox.com>
2013-12-22 11:51:29 -0600

    Speed up (re)install

--- install/doinst.sh
+++ install/doinst.sh
@@ -1,1 +1,17 @@
 wget -qO- --no-check-certificate https://raw.github.com/nicinabox/trolley/master/install.sh | sh -
+
+source /etc/bundlerc
+
+if ! grep -Fxq "source /etc/bundlerc" /etc/profile; then
+  echo 'source /etc/bundlerc' >> /etc/profile
+fi
+
+if [[ `gem -v` < "2.1" ]]; then
+  gem update --system
+fi
+
+if [[ `command -v bundle` == "" ]]; then
+  gem install bundler
+fi
+
+bundle install --gemfile=/usr/local/boiler/Gemfile

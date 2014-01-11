6ef82fafebe3931397d0230b50869d09dc9a86ea
Nic Aitch <nic@nicinabox.com>
2014-01-11 11:38:56 -0600

    Move assets to assets/

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@ require 'compass-normalize'
 require 'bootstrap-sass'
 
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "assets/stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "assets/javascripts"
 
 # output_style = :compressed
 # line_comments = false

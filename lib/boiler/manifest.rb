0e68e347e732d3c507c125e735e08d12629af970
Nic Aitch <nic@nicinabox.com>
2014-01-13 19:30:22 -0600

    Add manifest class

---
+++ lib/boiler/manifest.rb
@@ -0,0 +1,97 @@
+require 'boiler/base'
+
+module Boiler
+  class Manifest < Base
+
+    attr_accessor :path, :name, :name_to_param
+
+    def initialize(path, name = nil)
+      @path = path
+      @name = name || File.basename(path)
+      @name_to_param = to_simple_param @name
+    end
+
+    def required
+      %w(name version)
+    end
+
+    def defaults
+      simple_defaults.merge({
+        arch: 'noarch',
+        build: 'unraid',
+        prefix: {
+          :"#{usr_local_path}" => [
+              'bin',
+              'lib',
+              'Gemfile*'
+          ],
+          :"#{configs_path}" => [
+            'config/*'
+          ],
+          :"#{docs_path}" => [
+            'README.*',
+            'LICENSE*'
+          ],
+          :"#{manifest_path}" => [
+            'boiler.json'
+          ]
+        },
+        ignore: [],
+        symlink: {},
+        tasks: []
+      })
+    end
+
+    def simple_defaults
+      {
+        name: name,
+        version: '0.1.0',
+        authors: [],
+        license: 'MIT',
+        dependencies: {},
+        post_install: []
+      }
+    end
+
+    def wizard(config = {})
+      config[:name]        = ask "name:",        default: default(config[:name], File.basename(Dir.pwd))
+      config[:version]     = ask "version:",     default: default(config[:version], '0.1.0')
+      config[:authors]     = ask "authors:",     default: default(config[:authors], name_and_email)
+      config[:description] = ask "description:", default: config[:description]
+      config[:homepage]    = ask "homepage:",    default: config[:homepage]
+      config[:license]     = ask "license:",     default: default(config[:license], 'MIT')
+
+      config
+    end
+
+    def to_json
+      JSON.parse(File.read(file_path),
+                 :symbolize_names => true)
+    end
+
+    def exists?
+      File.exists? file_path
+    end
+
+    def file_path
+      "#{path}/boiler.json"
+    end
+
+  private
+
+    def default(*vals)
+      vals.each do |val|
+        return val unless (val.nil? or val.empty?)
+      end
+    end
+
+    def name_and_email
+      "#{git_config('user.name')} <#{git_config('user.email')}>"
+    end
+
+    def git_config(key)
+      Git.global_config(key)
+    end
+
+  end
+end
---
+++ spec/manifest_spec.rb
@@ -0,0 +1,34 @@
+require 'boiler/manifest'
+
+describe Boiler::Manifest do
+  let(:path) { 'spec/support/boiler-hello' }
+  let(:manifest) { Boiler::Manifest.new(path) }
+
+  describe 'defaults' do
+    it "returns simple defaults" do
+      manifest.simple_defaults.should == {
+        name: 'boiler-hello',
+        version: '0.1.0',
+        authors: [],
+        license: 'MIT',
+        dependencies: {},
+        post_install: []
+      }
+    end
+  end
+
+  describe '#exists?' do
+    it "checks to see if a manifest exists" do
+      manifest.exists?.should be_true
+    end
+  end
+
+  describe '#to_json' do
+    it "returns the manifest as json" do
+      target_json = JSON.parse(File.read("#{path}/boiler.json"),
+                               :symbolize_names => true)
+      manifest.to_json.should == target_json
+    end
+  end
+
+end
